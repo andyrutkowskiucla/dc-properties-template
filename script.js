54f82a0113afe0da02a5515c55a031ed08703755
Matthew Kenny <matthewkenny@gmail.com>
2013-07-11 09:56:01 -0700

    hack hack hack

--- script.js
+++ script.js
@@ -1,46 +1,25 @@
 var TileJSONs = [
-    'http://a.tiles.mapbox.com/v3/examples.map-20v6611k,mapbox.dc-property-values.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-building.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-construction.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-crime.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-crime-robbery.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-crime-theft.jsonp',
-    'http://a.tiles.mapbox.com/v3/mapbox.dc-crime-auto.jsonp',
-    'http://a.tiles.mapbox.com/v3/mattmakesmaps.FUDS_AK_V2.jsonp'
+    'http://a.tiles.mapbox.com/v3/mattmakesmaps.ak_native_placenames.jsonp',
 ];
 
 $('#map').mapbox(TileJSONs, function(map, tiledata) {
 
     // Assign readable names to all layers
-    map.getLayerAt(0).named('base');
-    map.getLayerAt(1).named('building');
-    map.getLayerAt(2).named('construction');
-    map.getLayerAt(3).named('crime');
-    map.getLayerAt(4).named('robbery');
-    map.getLayerAt(5).named('theft');
-    map.getLayerAt(6).named('auto');
-    map.getLayerAt(7).named('ak_fuds');
-
-    // Don't composite base layer with other layers
-    map.getLayer('base').composite(false);
+    map.getLayerAt(1).named('Place_Names');
 
     // Disable all overlay layers by default
-    map.disableLayer('building');
-    map.disableLayer('construction');
-    map.disableLayer('crime');
-    map.disableLayer('robbery');
-    map.disableLayer('theft');
-    map.disableLayer('auto');
-    map.disableLayer('ak_fuds');
+    map.disableLayer('Place_Names');
 
     // Set initial latitude, longitude and zoom level
+    // -145.2832,64.6427,4
+
     map.setCenterZoom({
-        lat: 38.90900,
-        lon: -77.01439
-    }, 13);
+        lat: 64.6,
+        lon: -145.2
+    }, 5);
 
     // Set minimum and maximum zoom levels
-    map.setZoomRange(0, 15);
+    map.setZoomRange(4, 8);
 
     // Enable share control
     mapbox.share().map(map).add();
