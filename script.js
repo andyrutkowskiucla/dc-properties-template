42228a62e6b7b2bbf283a2d355ad2cedd8069cc8
Matthew Kenny <matthewkenny@gmail.com>
2013-07-11 16:42:02 -0700

    Added basic layout and connections for AK Place Names Dataset.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 var TileJSONs = [
-    'http://a.tiles.mapbox.com/v3/mattmakesmaps.ak_native_placenames.jsonp',
+    'http://a.tiles.mapbox.com/v3/mattmakesmaps.map-9ipoh344.jsonp',
 ];
 
 $('#map').mapbox(TileJSONs, function(map, tiledata) {
 
     // Assign readable names to all layers
-    map.getLayerAt(1).named('Place_Names');
+    map.getLayerAt(0).named('Place_Names');
 
     // Disable all overlay layers by default
-    map.disableLayer('Place_Names');
+    //map.disableLayer('Place_Names');
 
     // Set initial latitude, longitude and zoom level
     // -145.2832,64.6427,4
